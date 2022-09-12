19a539963cd09a0a7027e4c7a03b4c12b42bcf01
hazem-smawy <hazemsmawy@gmail.com>
2022-09-12 14:40:41 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by hazem smawy on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
